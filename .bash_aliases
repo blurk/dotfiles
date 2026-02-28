2488723271ed7fcbe711fcd9d0cd91937b70c18b
blurk <nsinh6745@gmail.com>
2026-02-28 22:07:13 +0700

    update with stow

--- .bash_aliases
+++
@@ -1,6 +0,0 @@
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias cls='clear'
-alias my-update='cd ~/dotfiles/ && ./update.sh'
-
--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,8 @@ vim.o.relativenumber = true
 vim.o.ruler = true
 vim.o.hlsearch = true
 vim.o.wrap = true
+vim.o.clipboard = "unnamedplus"
 
 -- Keybindings
 vim.g.mapLeader = " "
+
