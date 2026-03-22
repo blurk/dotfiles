414a237067d0ba4750f85303826aad19ff69f786
blurk <nsinh6745@gmail.com>
2026-03-22 18:55:51 +0700

    update vim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,29 @@
 -- Editor
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.ruler = true
-vim.o.hlsearch = true
-vim.o.wrap = true
-vim.o.clipboard = "unnamedplus"
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ruler = true
+vim.opt.hlsearch = true
+vim.opt.wrap = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.cursorline = true
 
 -- Keybindings
 vim.g.mapleader = " "
 
 -- Map
-vim.keymap.set('n', '<leader>e', ':Ex<CR>', { silent = true })
+vim.keymap.set('n', '<leader>w', ':w<CR>', { silent = true })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { silent = true })
+vim.keymap.set('n', '<leader>r', ':source $MYVIMRC<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>l', '$', { noremap = true })
+vim.keymap.set('n', '<leader>h', '^', { noremap = true })
+
+vim.keymap.set('n', '<leader>e', function() 
+if vim.bo.filetype == "netrw" then
+  vim.cmd("bd")
+else
+  vim.cmd("Explore")
+end
+end, { silent = true, noremap = true })
